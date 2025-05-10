89d7f6319fa8a5a15110d34dbc38f2edefbedbe6
VikraSelpian <vikraselpian@gmail.com>
2025-05-10 12:42:17 +0700

    mengubah setting.py sementara

--- PENGENALAN/PENGENALAN/settings.py
+++ PENGENALAN/PENGENALAN/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'PENGENALAN.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'plat nomor',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
