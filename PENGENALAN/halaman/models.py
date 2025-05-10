5af31ece3ae21d05f855a43ccd2c343f6b134fa1
VikraSelpian <vikraselpian@gmail.com>
2025-05-10 15:00:14 +0700

    mengubah models.py yng ada dihalaman

--- PENGENALAN/halaman/models.py
+++ PENGENALAN/halaman/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Pelanggaran(models.Model):
+    gambar = models.ImageField(upload_to='pelanggaran/')
+    waktu = models.DateTimeField(auto_now_add=True)
+    lokasi = models.CharField(max_length=100)
+    plat_nomor = models.CharField(max_length=20)
+    status = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.plat_nomor
