d477fb23a837ca3a73a58b2f97d38ad68235e40b
seviamutiara <150576403+seviamutiara@users.noreply.github.com>
2025-07-09 17:30:54 +0700

    revisi deteksi

--- django-backend/PENGENALAN/settings.py
+++ django-backend/PENGENALAN/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-sxk=+a-ip+9h6l3sm9b)k2o73d0m)u2cn-0%xuo5du6+ellfjh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.10.6.125', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['10.10.0.127', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -87,7 +87,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
-        'PORT': '3307',
+        'PORT': '3306',
     }
 }
 
